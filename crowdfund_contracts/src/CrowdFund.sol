9daeca162d70f534dc42372cfe11fa825659d191
Aditi Singh <aditisingh01299@gmail.com>
2025-08-06 20:40:45 +0530

    contracts

--- crowdfund_contracts/src/CrowdFund.sol
+++ crowdfund_contracts/src/CrowdFund.sol
@@ -4,15 +4,70 @@ pragma solidity ^0.8.13;
 contract CrowdFund {
     struct Campaign {
         address owner;
+        string title;
+        string description;
+        uint256 target;
+        uint256 deadline;
+        uint256 amountCollected;
+        string image;
+        address[] donators;
+        uint256[] donations;
     }
 
-    function createCampaign() public returns (uint256) {}
+    uint256 public numberOfCampaigns = 0;
+    mapping(uint256 => Campaign) public campaigns;
 
-    function donateToCampaign() public payable {}
+    function createCampaign(
+        address _owner,
+        string memory _title,
+        string memory _description,
+        uint256 _target,
+        uint256 _deadline,
+        string memory _image
+    ) public returns (uint256) {
+        require(_deadline > block.timestamp, "Deadline must be in the future.");
 
-    function getDonators() public view returns (address[] memory) {}
+        Campaign storage campaign = campaigns[numberOfCampaigns];
 
-    function getCampaigns() public view returns (Campaign[] memory) {}
+        campaign.owner = _owner;
+        campaign.title = _title;
+        campaign.description = _description;
+        campaign.target = _target;
+        campaign.deadline = _deadline;
+        campaign.amountCollected = 0;
+        campaign.image = _image;
 
-    function getCampaign() public view returns (Campaign memory) {}
+        numberOfCampaigns++;
+
+        return numberOfCampaigns - 1;
+    }
+
+    function donateToCampaign(uint256 _id) public payable {
+        uint256 amount = msg.value;
+        Campaign storage campaign = campaigns[_id];
+
+        campaign.donators.push(msg.sender);
+        campaign.donations.push(amount);
+
+        (bool sent, ) = payable(campaign.owner).call{value: amount}("");
+        require(sent, "Transfer failed.");
+
+        campaign.amountCollected += amount;
+    }
+
+    function getDonators(uint256 _id) public view returns (address[] memory) {
+        return campaigns[_id].donators;
+    }
+
+    function getCampaigns() public view returns (Campaign[] memory) {
+        Campaign[] memory allCampaigns = new Campaign[](numberOfCampaigns);
+        for (uint256 i = 0; i < numberOfCampaigns; i++) {
+            allCampaigns[i] = campaigns[i];
+        }
+        return allCampaigns;
+    }
+
+    function getCampaign(uint256 _id) public view returns (Campaign memory) {
+        return campaigns[_id];
+    }
 }
